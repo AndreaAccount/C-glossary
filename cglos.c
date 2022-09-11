da667c8738a4b4847ba84df4b659f07a0fc9ae90
Andrea Mar <94307519+AndreaAccount@users.noreply.github.com>
2022-09-11 17:12:43 +0200

    Add files via upload

---
+++ cglos.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char choice;
+	int lsel;
+	printf("%s", "C GLOSSARY\nInstructions' syntax list and definitions\n\n----------------------------------------------\n");
+	printf("%s", "\nType:\n- 'L' to display the list of instructions currently \n  covered in this glossary.");
+	printf("%s", "\n- An instruction's number (read the list) to display its syntax.");
+	printf("%s", "\n- 'E' to exit the program\n");
+	scanf("%c", &choice);
+	if(choice == 'L' || choice == 'l')
+	{
+		printf("%s", "Variables:\n 1 - integer\n 2 - short\n 3 -  ");
+	}
+	if(choice == 'E' || choice == 'e')
+		return 0;
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
